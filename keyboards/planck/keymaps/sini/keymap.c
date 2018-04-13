224ccfce93a129a9faf893526d29a6893f23bf10
Jason Bowman <jbowman@uber.com>
2018-04-13 09:47:11 -0700

    transpose chars

--- keyboards/planck/keymaps/sini/keymap.c
+++ keyboards/planck/keymaps/sini/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC},
   {KC_ESC,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
-  {KC_LCTL, KC_LGUI, KC_BSLS, KC_LALT, LOWER, KC_SPC,  KC_SPC,   RAISE,  KC_LBRC, KC_RBRC, KC_EQL, CTL_T(KC_MINS)}
+  {KC_LCTL, KC_LGUI, KC_BSLS, KC_LALT, LOWER, KC_SPC,  KC_SPC,   RAISE,  KC_LBRC, KC_RBRC, KC_MINS, CTL_T(KC_EQL)}
 },
 
 
